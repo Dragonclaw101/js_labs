64fe6859fec8d17d941bde9ee6754f4de77b9d66
Dragonclaw101 <Huppjoesaphean@gmail.com>
2021-03-09 12:16:07 -0800

    Added lab 1.

---
+++ scripts/script.js
@@ -0,0 +1,7 @@
+'use strict';
+
+//Q: How does JavaScript aid in the building of a webpage?
+
+//Q: How do I use JavaScript to animate images?
+
+//Q: Can JavaScript read files on your local computer to run operations?
