c7f0439f4d7e3e792c91d0c3cd95bd170823a90f
Dragonclaw101 <Huppjoesaphean@gmail.com>
2021-03-09 12:26:48 -0800

    Added next lab.

--- scripts/script.js
+++ scripts/script.js
@@ -2,8 +2,26 @@
 
 let num = 73;
 let drink = 'water';
-let name = 'Peter';
+let named = 'Peter';
 
+
+function hardCode(){
 console.log('The number I am thinking of is ' + num);
 console.log('I like to drink ' + drink);
-console.log('My name is ' + name);
+console.log('My name is ' + named);
+}
+
+function input (number, drinks, names){
+    num = number;
+    drink = drinks;
+    named = names;
+
+
+    console.log('The number you picked is ' + num);
+    console.log('You like to drink ' + drink);
+    console.log('Your name is ' + named);
+}
+
+hardCode();
+
+input(13, 'lemonade', 'Mary');
