51055b3655180cde19eeb68968620abef82ea47c
Jo-Jo Hupp <huppjoesaphean@gmail.com>
2021-04-02 12:33:12 -0700

    Added Lab 52

--- scripts/script.js
+++ scripts/script.js
@@ -2,16 +2,11 @@
 
 //Arrays
 
-let myGames = ['Assassin\'s Creed', 'Fable', 'Legend of Zelda', 'Sly Cooper', 'Minecraft', '7 Days To Die', 'Plague Inc.', 'Rebel Inc.', 'DOOM', 'Need 4 Speed'];
+let myNums = [4, 232, 13, 23, 71, 2, 20, 43, 12, 19];
 
-console.log(myGames[0]);
-console.log(myGames[9]);
-console.log(myGames[0] + ' ' + myGames[9]);
-
-let myNums = [7, 112, 3, 13, 77];
-
-let sum = myNums[1] + myNums[2];
-let product = myNums[0] * myNums[1] * myNums[4];
-
-console.log(sum);
-console.log(product);
+for (let i = 0; i < myNums.length; i++){
+	let mySqr = myNums[i] * myNums[i];
+	
+	console.log(mySqr);
+	
+}
